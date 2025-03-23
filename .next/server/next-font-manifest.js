105f2e5bc2ea2ed1d4a853739058a71e89ac7bc6
Ronald Bardalez <ronald@bardalez.pe>
2025-03-23 02:58:21 -0500

    Remove .next folder from tracking

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/ronaldbardalez/Dev/superannotate/aurora/v0-item-00101-events-planner/app/layout\":[\"static/media/a34f9d1faa5f3315-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
